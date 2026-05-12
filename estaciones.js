da070cf81cde9a8e382e7b51548ccd313c3b68f0
lInightshadowIl <71063841+lInightshadowIl@users.noreply.github.com>
2026-05-12 19:13:58 -0400

    Update estaciones.js

--- estaciones.js
+++ estaciones.js
@@ -1,33 +1,33 @@
 const estacionesEFE = {
     "Línea 1": [
-        { id: "11", nombre: "Hualqui" },
-        { id: "12", nombre: "La Leonera" },
-        { id: "13", nombre: "Manquimavida" },
-        { id: "14", nombre: "Pedro Medina" },
-        { id: "15", nombre: "Chiguayante" },
-        { id: "16", nombre: "Concepción" }, // Combinación
-        { id: "17", nombre: "Lzo. Arenas" },
-        { id: "18", nombre: "UTFSM" },
-        { id: "19", nombre: "Los Condores" },
-        { id: "20", nombre: "Higueras" },
-        { id: "21", nombre: "El Arenal" },
-        { id: "22", nombre: "Mercado Thno." }
+        { id: "11", lat: -36.980714084, lng: -72.941098257, nombre: "Hualqui" },
+        { id: "12", lat: -36.951455412, lng: -73.013185126, nombre: "La Leonera" },
+        { id: "13", lat: -36.940102578, lng: -73.010704854, nombre: "Manquimavida" },
+        { id: "14", lat: -36.9255077,   lng: -73.0216289,   nombre: "Pedro Medina" },
+        { id: "15", lat: -36.9150817,   lng: -73.0292918,   nombre: "Chiguayante" },
+        { id: "16", lat: -36.8296198,   lng: -73.061125,    nombre: "Concepción" }, // Combinación
+        { id: "17", lat: -36.808438814, lng: -73.070914616, nombre: "Lzo. Arenas" },
+        { id: "18", lat: -36.7834898,   lng: -73.085283,    nombre: "UTFSM" },
+        { id: "19", lat: -36.756966,    lng: -73.091236,    nombre: "Los Condores" },
+        { id: "20", lat: -36.739319,    lng: -73.102973,    nombre: "Higueras" },
+        { id: "21", lat: -36.725351116, lng: -73.116710193, nombre: "El Arenal" },
+        { id: "22", lat: -36.711960915, lng: -73.115463145, nombre: "Mercado Thno." }
     ],
     "Línea 2": [
-        // Aunque Concepción (16) es parte de la L2 física, 
+        // Aunque Concepción (16) es parte de la L2 física,
         // el sistema la lista en el grupo 1. La app la unificará igual.
-        { id: "23", nombre: "Juan Pablo II" },
-        { id: "24", nombre: "Diagonal Biobio" },
-        { id: "25", nombre: "Alborada" },
-        { id: "26", nombre: "Costa Mar" },
-        { id: "27", nombre: "El Parque" },
-        { id: "28", nombre: "Lomas Coloradas" },
-        { id: "29", nombre: "Cardenal Raúl Silva H." },
-        { id: "30", nombre: "Hito Galvarino" },
-        { id: "31", nombre: "Los Canelos" },
-        { id: "32", nombre: "Huinca" },
-        { id: "33", nombre: "Cristo Redentor" },
-        { id: "34", nombre: "Laguna Quiñenco" },
-        { id: "35", nombre: "Intermodal Coronel" }
+        { id: "23", lat: -36.8387269,   lng: -73.09712,     nombre: "Juan Pablo II" },
+        { id: "24", lat: -36.8374178,   lng: -73.1179909,   nombre: "Diagonal Biobio" },
+        { id: "25", lat: -36.8466064,   lng: -73.1309347,   nombre: "Alborada" },
+        { id: "26", lat: -36.855017,    lng: -73.1328093,   nombre: "Costa Mar" },
+        { id: "27", lat: -36.8680406,   lng: -73.1352565,   nombre: "El Parque" },
+        { id: "28", lat: -36.882732889, lng: -73.139622495, nombre: "Lomas Coloradas" },
+        { id: "29", lat: -36.8982835,   lng: -73.1413321,   nombre: "Cardenal Raúl Silva H." },
+        { id: "30", lat: -36.9533195,   lng: -73.1545593,   nombre: "Hito Galvarino" },
+        { id: "31", lat: -36.9798198,   lng: -73.1604823,   nombre: "Los Canelos" },
+        { id: "32", lat: -36.9877651,   lng: -73.160903,    nombre: "Huinca" },
+        { id: "33", lat: -36.9982793,   lng: -73.1606352,   nombre: "Cristo Redentor" },
+        { id: "34", lat: -37.0121383,   lng: -73.1568824,   nombre: "Laguna Quiñenco" },
+        { id: "35", lat: -37.023597178, lng: -73.152215829, nombre: "Intermodal Coronel" }
     ]
 };
