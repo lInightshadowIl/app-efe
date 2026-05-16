75727c299c58e0be36c019fa22815ebb02dd8ef2
lInightshadowIl <71063841+lInightshadowIl@users.noreply.github.com>
2026-05-16 02:01:23 -0400

    Update estaciones.js

--- estaciones.js
+++ estaciones.js
@@ -1,28 +1,28 @@
 const estacionesEFE = {
     "Línea 1": [
-        { id: "11", lat: -36.980714084, lng: -72.941098257, nombre: "Hualqui" },
-        { id: "12", lat: -36.951455412, lng: -73.013185126, nombre: "La Leonera" },
-        { id: "13", lat: -36.940102578, lng: -73.010704854, nombre: "Manquimavida" },
-        { id: "14", lat: -36.9255077,   lng: -73.0216289,   nombre: "Pedro Medina" },
-        { id: "15", lat: -36.9150817,   lng: -73.0292918,   nombre: "Chiguayante" },
-        { id: "16", lat: -36.8296198,   lng: -73.061125,    nombre: "Concepción" }, // Combinación
-        { id: "17", lat: -36.808438814, lng: -73.070914616, nombre: "Lzo. Arenas" },
-        { id: "18", lat: -36.7834898,   lng: -73.085283,    nombre: "UTFSM" },
-        { id: "19", lat: -36.756966,    lng: -73.091236,    nombre: "Los Condores" },
-        { id: "20", lat: -36.739319,    lng: -73.102973,    nombre: "Higueras" },
-        { id: "21", lat: -36.725351116, lng: -73.116710193, nombre: "El Arenal" },
-        { id: "22", lat: -36.711960915, lng: -73.115463145, nombre: "Mercado Thno." }
+        { id: "11", lat: -36.98049863938148, lng: -72.94101508545344, nombre: "Hualqui" },
+        { id: "12", lat: -36.95143673397818, lng: -73.01318282285652, nombre: "La Leonera" },
+        { id: "13", lat: -36.93824143232025, lng: -73.01138171954784, nombre: "Manquimavida" },
+        { id: "14", lat: -36.92555396222054,   lng: -73.02180904941594,   nombre: "Pedro Medina" },
+        { id: "15", lat: -36.91519246970432,   lng: -73.02920121749106,   nombre: "Chiguayante" },
+        { id: "16", lat: -36.82966984581784,   lng: -73.06135190563577,    nombre: "Concepción" }, // Combinación
+        { id: "17", lat: -36.80806460598207, lng: -73.07133505042096, nombre: "Lzo. Arenas" },
+        { id: "18", lat: -36.78361756337353,   lng: -73.08559597334344,    nombre: "UTFSM" },
+        { id: "19", lat: -36.75694730277074,    lng: -73.09175661180774,    nombre: "Los Condores" },
+        { id: "20", lat: -36.73934460459696,    lng: -73.10299526928867,    nombre: "Higueras" },
+        { id: "21", lat: -36.72515255012395, lng: -73.11694997522551, nombre: "El Arenal" },
+        { id: "22", lat: -36.711823930979364, lng: -73.11545168011612, nombre: "Mercado Thno." }
     ],
     "Línea 2": [
         // Aunque Concepción (16) es parte de la L2 física,
         // el sistema la lista en el grupo 1. La app la unificará igual.
-        { id: "23", lat: -36.8387269,   lng: -73.09712,     nombre: "Juan Pablo II" },
-        { id: "24", lat: -36.8374178,   lng: -73.1179909,   nombre: "Diagonal Biobio" },
-        { id: "25", lat: -36.8466064,   lng: -73.1309347,   nombre: "Alborada" },
-        { id: "26", lat: -36.855017,    lng: -73.1328093,   nombre: "Costa Mar" },
-        { id: "27", lat: -36.8680406,   lng: -73.1352565,   nombre: "El Parque" },
-        { id: "28", lat: -36.882732889, lng: -73.139622495, nombre: "Lomas Coloradas" },
-        { id: "29", lat: -36.8982835,   lng: -73.1413321,   nombre: "Cardenal Raúl Silva H." },
+        { id: "23", lat: -36.8385521150118,   lng: -73.09749196653688,     nombre: "Juan Pablo II" },
+        { id: "24", lat: -36.83735856978956,   lng: -73.118262993157,   nombre: "Diagonal Biobio" },
+        { id: "25", lat: -36.84647148892099,   lng: -73.13096251780864,   nombre: "Alborada" },
+        { id: "26", lat: -36.85497349064785,   lng: -73.13312553999198,   nombre: "Costa Mar" },
+        { id: "27", lat: -36.86773316135933,   lng: -73.13588566428788,   nombre: "El Parque" },
+        { id: "28", lat: -36.8830763448669, lng: -73.1397845168883, nombre: "Lomas Coloradas" },
+        { id: "29", lat: -36.89821276546122,   lng: -73.1434269568075,   nombre: "Cardenal Raúl Silva H." },
         { id: "30", lat: -36.953874415430725,  lng: -73.1567433178572,   nombre: "Hito Galvarino" },
         { id: "31", lat: -36.97990642104991,   lng: -73.16271623873433,  nombre: "Los Canelos" },
         { id: "32", lat: -36.98861389479508,   lng: -73.16309707904992,  nombre: "Huinca" },
